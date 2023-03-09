21675603c40ee4c500acd5cd2220813aad31d83f
Mohammed <m97.chahboun@gmail.com>
2023-03-09 13:13:12 +0100

    Added compononts to app root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ButtonDisabledComponent } from './button-disabled/button-disabled.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    ButtonDisabledComponent
   ],
   imports: [
     BrowserModule,
